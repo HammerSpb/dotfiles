69f9fa4f84bddffc9dbadbc9a76312f9d9061fc1
Roman Fedoseev <Fedoseev.Roman@gmail.com>
2019-09-19 14:53:16 -0400

    Remove bin

--- bin/install.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/sh
-# Author: Roman Fedoseev (Fedoseev.Roman@gmail.com)
-# Description: The following script is the part of set of .dotfiles
-source $DOTFILES/scripts/utils.sh
-
-echo "Installing commands..."
-
-# Symlink this directory to home
-ln -s $DOTFILES/bin $HOME/.bin
