b8681ebccd15e1df43aab9d474f60a3676b1fe5b
Roman Fedoseev <Fedoseev.Roman@gmail.com>
2019-09-19 15:03:09 -0400

    Update zsh

--- zsh/install.sh
+++ zsh/install.sh
@@ -9,6 +9,7 @@ brew_install_arr ${zsh_packages[@]}
 
 info 'Setting up zsh as the default shell'
 # sudo sh -c $(which zsh) >> /etc/shells
+echo 'export PATH="/usr/local/bin:$PATH"' >> $HOME/.zshrc
 chsh -s $(which zsh)
 
 success 'Zsh was successfully installed!'
